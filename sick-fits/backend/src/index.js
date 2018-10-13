189bb94e5bc62f553973363fc0a3864cb45dce88
ben <wbpiggot@gmail.com>
2018-10-13 09:02:42 -0700

    starting to add graphql yoga server architecture

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,10 @@
 // let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// use express middleware to handle cookies (JWT)
+
+// use express middleware to populate current user
