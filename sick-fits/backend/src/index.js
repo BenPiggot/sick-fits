bd5827b8de4bfe80834680ffd08d25024ebb9703
ben <wbpiggot@gmail.com>
2018-10-13 20:35:14 -0700

    wip

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -5,6 +5,16 @@ const db = require('./db');
 
 const server = createServer();
 
-// use express middleware to handle cookies (JWT)
+// TODO use express middleware to handle cookies (JWT)
+// TODO use express middleware to populate current user
 
-// use express middleware to populate current user
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+}, (res) => {
+  console.log(`Server running on port ${res.port}`)
+})
+
+
