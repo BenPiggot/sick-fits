2ac4b8b6c36b215373eaf6e09d3f91678b827024
ben <wbpiggot@gmail.com>
2019-01-10 23:04:06 -0800

    adding orders page

--- sick-fits/frontend/components/Order.js
+++ sick-fits/frontend/components/Order.js
@@ -6,7 +6,7 @@ import Head from 'next/head';
 import gql from 'graphql-tag';
 import formatMoney from '../lib/formatMoney';
 import Error from './ErrorMessage';
-import OrderStyles from './styles/OrderItemStyles';
+import OrderStyles from './styles/OrderStyles';
 
 const SINGLE_ORDER_QUERY = gql`
   query SINGLE_ORDER_QUERY($id: ID!) {
@@ -43,10 +43,47 @@ class Order extends Component {
         {({data, loading, error}) => {
           if (error) return <Error error={error}/>
           if (loading) return <p>Loading...</p>
+          const order = data.order;
           return (
-            <div>
-              <p>Order ID: {this.props.id} </p>
-            </div>
+            <OrderStyles>
+              <Head>
+                <title>Sick Fits - Order {order.id}</title>
+              </Head>
+              <p>
+                <span>Order ID: </span>
+                <span>{this.props.id} </span>
+              </p>
+              <p>
+                <span>Charge: </span>
+                <span>{order.charge}</span>
+              </p>
+              <p>
+                <span>Date: </span>
+                <span>{format(order.createdAt, 'MMMM D, YYYY h:mm a')}</span>
+              </p>
+              <p>
+                <span>Order Total: </span>
+                <span>{formatMoney(order.total)}</span>
+              </p>
+              <p>
+                <span>Item Count: </span>
+                <span>{order.items.length}</span>
+              </p>
+              <div className="items">
+                {order.items.map(item => (
+                  <div className="order-item" key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <div className="item-details">
+                      <h2>{item.title}</h2>
+                      <p>Qty: {item.quantity}</p>
+                      <p>Each: {formatMoney(item.price)}</p>
+                      <p>Subtotal: {formatMoney(item.price * item.quantity)}</p>
+                      <p>{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </OrderStyles>
           )
         }}
       </Query>
